709941ab017ca99ee09ab131d584d828fdeed58c
Adler Lima <adler_sl@hotmail.com>
2022-10-23 17:27:57 -0300

    feature: Adicionado tema claro e provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from '@emotion/react';
 import { useState } from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from './routes';
+import { LightTheme } from './shared/themes';
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <AppRoutes/>
-    </BrowserRouter>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
